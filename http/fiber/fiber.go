cac57adc7064aa7e2e74dc8f5105273937f15fec
João Fernandes <joaofnds@joaofnds.com>
2022-10-08 17:00:29 -0300

    feat: add recover middleware

--- http/fiber/fiber.go
+++ http/fiber/fiber.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"go.uber.org/fx"
 )
 
@@ -22,6 +23,7 @@ type HTTPInstrumentation interface {
 
 func NewFiber(instrumentation HTTPInstrumentation) *fiber.App {
 	app := fiber.New()
+	app.Use(recover.New())
 	app.Use(instrumentation.Middleware)
 	app.Use(cors.New())
 	return app
---
+++ http/fiber/recover_test.go
@@ -0,0 +1,54 @@
+package fiber_test
+
+import (
+	"astro/config"
+	"astro/http/fiber"
+	"astro/http/util"
+	"astro/test"
+	"fmt"
+	"net/http"
+
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxtest"
+
+	. "astro/test/matchers"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("fiber", func() {
+	var (
+		fxApp *fxtest.App
+		url   string
+	)
+
+	BeforeEach(func() {
+		var cfg config.AppConfig
+
+		fxApp = fxtest.New(
+			GinkgoT(),
+			test.NopLogger,
+			test.RandomAppConfigPort,
+			test.NopHTTPInstrumentation,
+			test.PanicHandler,
+			config.Module,
+			fiber.Module,
+			fx.Populate(&cfg),
+		).RequireStart()
+
+		url = fmt.Sprintf("http://localhost:%d", cfg.Port)
+	})
+
+	AfterEach(func() {
+		fxApp.RequireStop()
+	})
+
+	It("recovers from panic", func() {
+		req := Must2(util.Get(url+"/panic", nil))
+		Expect(req.StatusCode).To(Equal(http.StatusInternalServerError))
+
+		req = Must2(util.Get(url+"/somethingelse", nil))
+		Expect(req.StatusCode).To(Equal(http.StatusNotFound))
+	})
+})
