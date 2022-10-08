921041e86ce95c94e736f636cde4494d03520f5a
João Fernandes <joaofnds@joaofnds.com>
2022-10-08 18:16:44 -0300

    test: metrics endpoint

---
+++ metrics/metrics_test.go
@@ -0,0 +1,44 @@
+package metrics_test
+
+import (
+	"astro/http/util"
+	"astro/metrics"
+	"astro/test"
+	. "astro/test/matchers"
+	"io"
+	"net/http"
+	"testing"
+
+	"go.uber.org/fx/fxtest"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestHealth(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "/health suite")
+}
+
+var _ = Describe("/", Ordered, func() {
+	url := "http://localhost:9091/metrics"
+
+	BeforeAll(func() {
+		fxtest.New(
+			GinkgoT(),
+			test.NopLogger,
+			metrics.Module,
+		).RequireStart()
+	})
+
+	It("returns status OK", func() {
+		res := Must2(util.Get(url, nil))
+		Expect(res.StatusCode).To(Equal(http.StatusOK))
+	})
+
+	It("returns how many requests were made", func() {
+		res := Must2(util.Get(url, nil))
+		b := Must2(io.ReadAll(res.Body))
+		Expect(b).To(ContainSubstring(`promhttp_metric_handler_requests_total{code="200"} 1`))
+	})
+})
