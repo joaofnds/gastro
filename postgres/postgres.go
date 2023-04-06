7f2b5b05b7a8565f50936cedc2882c62c48b7911
João Fernandes <joaofnds@joaofnds.com>
2023-04-05 23:15:48 -0300

    refactor: join create tables

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -25,36 +25,14 @@ func NewClient(postgresConfig Config, logger *zap.Logger) (*sql.DB, error) {
 func HookConnection(lifecycle fx.Lifecycle, db *sql.DB, logger *zap.Logger) {
 	lifecycle.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
-			err := db.PingContext(ctx)
-			if err != nil {
+			if err := db.PingContext(ctx); err != nil {
 				logger.Error("failed to ping db", zap.Error(err))
 				return err
 			}
-
 			logger.Info("successfully pinged db")
 
-			if err = enableUUIDExtension(ctx, db); err != nil {
-				logger.Error("failed to enable uuid extension", zap.Error(err))
-				return err
-			}
-
-			if err = createHabitsTable(ctx, db); err != nil {
+			if err := createTables(ctx, db); err != nil {
 				logger.Error("failed to create habits table", zap.Error(err))
-				return err
-			}
-
-			if err = createActivitiesTable(ctx, db); err != nil {
-				logger.Error("failed to create activities table", zap.Error(err))
-				return err
-			}
-
-			if err = createGroupsTable(ctx, db); err != nil {
-				logger.Error("failed to create groups table", zap.Error(err))
-				return err
-			}
-
-			if err = createGroupsHabitsJoinTable(ctx, db); err != nil {
-				logger.Error("failed to create groups-habits join table", zap.Error(err))
 				return err
 			}
 
@@ -62,8 +40,7 @@ func HookConnection(lifecycle fx.Lifecycle, db *sql.DB, logger *zap.Logger) {
 		},
 
 		OnStop: func(ctx context.Context) error {
-			err := db.Close()
-			if err != nil {
+			if err := db.Close(); err != nil {
 				logger.Error("failed to close db connection", zap.Error(err))
 				return err
 			}
@@ -73,58 +50,40 @@ func HookConnection(lifecycle fx.Lifecycle, db *sql.DB, logger *zap.Logger) {
 	})
 }
 
-func createHabitsTable(ctx context.Context, db *sql.DB) error {
+func createTables(ctx context.Context, db *sql.DB) error {
 	_, err := db.ExecContext(ctx, `
-		CREATE TABLE IF NOT EXISTS habits (
-			id   UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-			user_id UUID NOT NULL,
-			name VARCHAR NOT NULL
-		);
-		CREATE UNIQUE INDEX IF NOT EXISTS idx_habit_id_and_user_id ON habits(id, user_id);
-	`)
-	return err
-}
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-func createActivitiesTable(ctx context.Context, db *sql.DB) error {
-	_, err := db.ExecContext(ctx, `
-		CREATE TABLE IF NOT EXISTS activities (
-			id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-			habit_id    UUID NOT NULL REFERENCES habits(id) ON DELETE CASCADE,
-			description VARCHAR NOT NULL DEFAULT '',
-			created_at  TIMESTAMP NOT NULL DEFAULT NOW()
-		);
-		CREATE INDEX IF NOT EXISTS idx_activity_habit ON activities(habit_id);
-	`)
-	return err
-}
+    CREATE TABLE IF NOT EXISTS habits (
+      id   UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      user_id UUID NOT NULL,
+      name VARCHAR NOT NULL
+    );
+    CREATE UNIQUE INDEX IF NOT EXISTS idx_habit_id_and_user_id ON habits(id, user_id);
 
-func createGroupsTable(ctx context.Context, db *sql.DB) error {
-	_, err := db.ExecContext(ctx, `
-        CREATE TABLE IF NOT EXISTS "public"."groups" (
-            id      UUID NOT NULL DEFAULT uuid_generate_v4(),
-            name    VARCHAR NOT NULL,
-			user_id UUID NOT NULL,
-            PRIMARY KEY (id, user_id)
-        );
-    `)
-	return err
-}
+    CREATE TABLE IF NOT EXISTS activities (
+      id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      habit_id    UUID NOT NULL REFERENCES habits(id) ON DELETE CASCADE,
+      description VARCHAR NOT NULL DEFAULT '',
+      created_at  TIMESTAMP NOT NULL DEFAULT NOW()
+    );
+    CREATE INDEX IF NOT EXISTS idx_activity_habit ON activities(habit_id);
 
-func createGroupsHabitsJoinTable(ctx context.Context, db *sql.DB) error {
-	_, err := db.ExecContext(ctx, `
-        CREATE TABLE IF NOT EXISTS groups_habits (
-            group_id UUID NOT NULL,
-            habit_id UUID NOT NULL,
-            user_id  UUID NOT NULL,
-            CONSTRAINT "groups_habits_habit_id_fkey" FOREIGN KEY (habit_id, user_id) REFERENCES habits (id, user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-            CONSTRAINT "groups_habits_group_id_fkey" FOREIGN KEY (group_id, user_id) REFERENCES groups (id, user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-            PRIMARY KEY (group_id, habit_id)
-        );
-    `)
-	return err
-}
+    CREATE TABLE IF NOT EXISTS "public"."groups" (
+      id      UUID NOT NULL DEFAULT uuid_generate_v4(),
+      name    VARCHAR NOT NULL,
+      user_id UUID NOT NULL,
+      PRIMARY KEY (id, user_id)
+    );
 
-func enableUUIDExtension(ctx context.Context, db *sql.DB) error {
-	_, err := db.ExecContext(ctx, `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
+    CREATE TABLE IF NOT EXISTS groups_habits (
+      group_id UUID NOT NULL,
+      habit_id UUID NOT NULL,
+      user_id  UUID NOT NULL,
+      CONSTRAINT "groups_habits_habit_id_fkey" FOREIGN KEY (habit_id, user_id) REFERENCES habits (id, user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+      CONSTRAINT "groups_habits_group_id_fkey" FOREIGN KEY (group_id, user_id) REFERENCES groups (id, user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+      PRIMARY KEY (group_id, habit_id)
+    );
+  `)
 	return err
 }
