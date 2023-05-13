8df423bc1d17f6bce24d572e6e2eac9af4ba38d8
João Fernandes <joaofnds@joaofnds.com>
2023-05-13 01:03:38 -0300

    refactor: rename files

---
+++ habit/prom_probe.go
@@ -0,0 +1,31 @@
+package habit
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"go.uber.org/zap"
+)
+
+type PromProbe struct {
+	logger             *zap.Logger
+	habitsCreated      prometheus.Counter
+	habitsCreateFailed prometheus.Counter
+}
+
+func NewPromProbe(logger *zap.Logger) *PromProbe {
+	return &PromProbe{
+		logger:             logger,
+		habitsCreated:      promauto.NewCounter(prometheus.CounterOpts{Name: "astro_habits_created"}),
+		habitsCreateFailed: promauto.NewCounter(prometheus.CounterOpts{Name: "astro_habits_create_fail"}),
+	}
+}
+
+func (p *PromProbe) LogFailedToCreateHabit(err error) {
+	p.logger.Info("failed to create habit", zap.Error(err))
+	p.habitsCreateFailed.Inc()
+}
+
+func (p *PromProbe) LogHabitCreated() {
+	p.logger.Info("habit created")
+	p.habitsCreated.Inc()
+}
