34fa0d218303f97101e512a3ae8b36ec45e7f0ff
João Fernandes <joaofnds@joaofnds.com>
2023-05-18 10:42:02 -0300

    refactor: embed schema

--- adapters/postgres/postgres.go
+++ adapters/postgres/postgres.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 	"database/sql"
+	_ "embed"
 
 	"go.uber.org/fx"
 	"go.uber.org/zap"
@@ -19,6 +20,9 @@ var Module = fx.Module(
 	fx.Provide(NewHealthChecker),
 	fx.Invoke(HookConnection),
 )
+
+//go:embed schema.sql
+var schema string
 
 func NewGormDB(postgresConfig Config, logger *zap.Logger) (*gorm.DB, error) {
 	return gorm.Open(
@@ -40,7 +44,7 @@ func HookConnection(lifecycle fx.Lifecycle, db *sql.DB, logger *zap.Logger) {
 			}
 			logger.Info("successfully pinged db")
 
-			if err := createTables(ctx, db); err != nil {
+			if _, err := db.ExecContext(ctx, schema); err != nil {
 				logger.Error("failed to create habits table", zap.Error(err))
 				return err
 			}
@@ -57,42 +61,4 @@ func HookConnection(lifecycle fx.Lifecycle, db *sql.DB, logger *zap.Logger) {
 			return nil
 		},
 	})
-}
-
-func createTables(ctx context.Context, db *sql.DB) error {
-	_, err := db.ExecContext(ctx, `
-    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-    CREATE TABLE IF NOT EXISTS habits (
-      id   UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-      user_id UUID NOT NULL,
-      name VARCHAR NOT NULL
-    );
-    CREATE UNIQUE INDEX IF NOT EXISTS idx_habit_id_and_user_id ON habits(id, user_id);
-
-    CREATE TABLE IF NOT EXISTS activities (
-      id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-      habit_id    UUID NOT NULL REFERENCES habits(id) ON DELETE CASCADE,
-      description VARCHAR NOT NULL DEFAULT '',
-      created_at  TIMESTAMP NOT NULL DEFAULT NOW()
-    );
-    CREATE INDEX IF NOT EXISTS idx_activity_habit ON activities(habit_id);
-
-    CREATE TABLE IF NOT EXISTS "public"."groups" (
-      id      UUID NOT NULL DEFAULT uuid_generate_v4(),
-      name    VARCHAR NOT NULL,
-      user_id UUID NOT NULL,
-      PRIMARY KEY (id, user_id)
-    );
-
-    CREATE TABLE IF NOT EXISTS groups_habits (
-      group_id UUID NOT NULL,
-      habit_id UUID NOT NULL,
-      user_id  UUID NOT NULL,
-      CONSTRAINT "groups_habits_habit_id_fkey" FOREIGN KEY (habit_id, user_id) REFERENCES habits (id, user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-      CONSTRAINT "groups_habits_group_id_fkey" FOREIGN KEY (group_id, user_id) REFERENCES groups (id, user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-      PRIMARY KEY (group_id, habit_id)
-    );
-  `)
-	return err
 }
