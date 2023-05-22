deb6251fb7d5eeb588bf0605c63c214aa897e3f6
João Fernandes <joaofnds@joaofnds.com>
2023-05-22 12:37:37 -0300

    refactor: reformat sql files

--- cmd/migrate/migrations/20230521004811_create_habits_table.sql
+++ cmd/migrate/migrations/20230521004811_create_habits_table.sql
@@ -1,11 +1,11 @@
 -- +goose Up
-CREATE TABLE habits(
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  user_id uuid NOT NULL,
-  name varchar NOT NULL
+CREATE TABLE habits (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid NOT NULL,
+    name varchar NOT NULL
 );
 
-CREATE UNIQUE INDEX idx_habit_id_and_user_id ON habits(id, user_id);
+CREATE UNIQUE INDEX idx_habit_id_and_user_id ON habits (id, user_id);
 
 -- +goose Down
 DROP TABLE habits;
--- cmd/migrate/migrations/20230521005335_create_groups_table.sql
+++ cmd/migrate/migrations/20230521005335_create_groups_table.sql
@@ -1,9 +1,9 @@
 -- +goose Up
-CREATE TABLE "groups"(
-  id uuid NOT NULL DEFAULT uuid_generate_v4(),
-  name varchar NOT NULL,
-  user_id uuid NOT NULL,
-  PRIMARY KEY (id, user_id)
+CREATE TABLE "groups" (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    name varchar NOT NULL,
+    user_id uuid NOT NULL,
+    PRIMARY KEY (id, user_id)
 );
 
 -- +goose Down
